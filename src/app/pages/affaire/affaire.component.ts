8d8904a0d0c884fa046ce3853b6fe98d04e7fedb
anthoLh <antho92220@hotmail.fr>
2019-09-03 16:24:39 +0200

    ajout interface affaire

--- src/app/pages/affaire/affaire.component.ts
+++ src/app/pages/affaire/affaire.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Affaire } from 'src/app/model/affaire';
+import { AffaireService } from 'src/app/service/affaire.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-affaire',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./affaire.component.scss']
 })
 export class AffaireComponent implements OnInit {
+  affaires: any[];
+  affaire: Affaire = new Affaire();
 
-  constructor() { }
+  constructor(private affaireService: AffaireService, private router: Router) { }
 
   ngOnInit() {
+    this.loadAffaire();
   }
+
+  loadAffaire(){
+    this.affaireService.getAllAffaire().subscribe(
+      data => {this.affaires = data; },
+      error => {console.log(error); }
+    )
+  }
+  createAffaire() {
+    this.affaireService.saveAffaire(this.affaire).subscribe(
+      () => { this.loadAffaire(); this.affaire = new Affaire(); }
+    )
+  }
+  deleteAffaire(affaire) {
+    this.affaireService.deleteAffaire(affaire.idAffaire).subscribe(
+      () => { this.loadAffaire(); },
+      error => { console.log(error); }
+    )
+  }
+
 
 }
